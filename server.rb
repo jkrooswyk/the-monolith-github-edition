0d7ad98fc9b4b113089bced5c34eb4e736f60aed
Joel Krooswyk <jkrooswyk@gitlab.com>
2017-09-11 14:34:49 +0000

    Change to CSS, centered with background color.

--- server.rb
+++ server.rb
@@ -5,9 +5,23 @@ server = WEBrick::HTTPServer.new :Port => 5000
 
 #Return a successful Hello World response.
 server.mount_proc '/' do |request, response|
-  response.body = 'oHell, world!'
+  style = '<style type="text/css" media="screen">'
+  style += 'body { background-color: slategray; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); color: white; font-size: 250%; }'
+  style += '</style>'
+  
+  message = 'oHell, world!'
+  
+  body = "<body>#{message}</body>"
+  
+  response.body = style + body
   sleep 0.125 + SecureRandom.random_number*0.250
 end
+
+#Return a successful Hello World response.
+#server.mount_proc '/' do |request, response|
+#  response.body = 'oHell, world!'
+#  sleep 0.125 + SecureRandom.random_number*0.250
+#end
 
 #Return an error to help demonstrate monitoring capabilities
 server.mount_proc '/error' do |request, response|
